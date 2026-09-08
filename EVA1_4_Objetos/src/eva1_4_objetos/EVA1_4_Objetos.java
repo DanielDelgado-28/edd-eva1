0c7508587ba6863b29cab82cba8d3e9bc60fc0e5
DanielDelgado-28 <l24550298@chihuahua2.tecnm.mx>
2026-09-08 16:06:02 -0600

    Add files via upload

---
+++ EVA1_4_Objetos/src/eva1_4_objetos/EVA1_4_Objetos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_objetos;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class EVA1_4_Objetos {
+    
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        System.out.println(prueba);
+        //ELIMINAR pruebas 
+        //Terminar el programa --> garbage collector(liberar memoria de manera automatica)
+        //eliminar nosotros el objeto
+        prueba = null;
+        
+    }
+    }
+    class Prueba{
+        
+    }
+    
+
